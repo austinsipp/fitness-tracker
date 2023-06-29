e89437ef833e1ceebedb3cdd1e1eecb74784ce57
austinsipp <austin.sipp@gmail.com>
2023-06-28 22:09:57 -0700

    add supabase library and be able to submit data to a table in the database

--- src/features/submitFoodSlice.js
+++ src/features/submitFoodSlice.js
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://mgksxejsytiekgoegymi.supabase.co'
+//const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1na3N4ZWpzeXRpZWtnb2VneW1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5MDY0ODIsImV4cCI6MjAwMzQ4MjQ4Mn0.8ZrhQ99WNx320VtwJpp59kGOUQc-ebwHaUeQIfl5_vQ"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 
 const initialState = {
     foodData: [{
@@ -19,6 +26,12 @@ export const submitFoodSlice = createSlice({
             console.log(action.payload)
             newState.foodData.push(action.payload)
             console.log("foodData:", newState.foodData)
+            
+            async function sendData (sentRecord) {
+                const response = await supabase.from('foodDataTest').insert(sentRecord)
+                console.log("response is:",response)
+            }
+            sendData(action.payload)
             debugger
             /*Object.keys(state).forEach((key) => {
                 if( key === itemName) {

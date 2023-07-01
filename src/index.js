d544b3e93db4666b1be726cd873334b40c1c019b
austinsipp <austin.sipp@gmail.com>
2023-07-01 13:05:09 -0700

    remove redux files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './stores/store'
-import { Provider } from 'react-redux'
+//import { store } from './stores/store'
+//import { Provider } from 'react-redux'
 //import dotenv from 'dotenv'
 //dotenv.config()
 /*const express = require('express')
@@ -17,9 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
--- src/stores/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import currentEntryReducer from '../features/submitFoodSlice'
-
-export const store = configureStore({
-    reducer: {
-        currentState: currentEntryReducer
-    }
-})
